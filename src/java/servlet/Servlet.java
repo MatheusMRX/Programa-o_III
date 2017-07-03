00252a081b1be4e3b2e7155c6f8183f00efdf5f4
MatheusMRX <matheusnunesenem123@gmail.com>
2017-07-03 13:05:26 -0300

    Projeto_III

--- src/java/servlet/Servlet.java
+++ src/java/servlet/Servlet.java
@@ -61,8 +61,6 @@ public class Servlet extends HttpServlet {
             
             dataFinal = (Dia + "/" + Mes + "/" + Ano);
         }
-        
-        System.out.println("Gravado");
 
     }
 
